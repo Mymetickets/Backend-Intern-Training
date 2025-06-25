cc6a7979645e176a6ab7b004549b1a6d5d8584ad
Willy-sam021 <s.o.williams021@gmail.com>
2025-06-25 14:31:09 +0100

    updated event controller
    returned successful api response

--- app/Http/Controllers/EventBookingController.php
+++ app/Http/Controllers/EventBookingController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class EventBookingController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+
+        return response()->json([
+            'message' => 'Event Created successfully',
+            
+        ]);
+    }
 }
