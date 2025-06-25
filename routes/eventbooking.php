71f6d936d76fc54d45c1ebc07b29afe9be752bc5
Willy-sam021 <s.o.williams021@gmail.com>
2025-06-25 14:17:00 +0100

    updated the event route file
    worked on the event booting route file

---
+++ routes/eventbooking.php
@@ -0,0 +1,11 @@
+<?php
+use App\Http\Controllers\EventBookingController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/event', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/event/booking', [EventBookingController::class, 'index'])
+
+    ->middleware('auth:api');
