04a84b930775f98569f3f6ffce949f4562bcb3cd
Willy-sam021 <s.o.williams021@gmail.com>
2025-06-25 14:08:39 +0100

    updated the app.php
    added the exception handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,4 +26,10 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
+    })
+
+     ->withExceptions(function (Exceptions $exceptions) {
+    $exceptions->report(function (InvalidOrderException $e) {
+        // ...
+    });
     })->create();
