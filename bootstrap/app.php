4a3217a4df4948f04a4207384c3395e68f63d00c
Absamis <smallkid1@yahoo.com>
2025-06-25 10:21:28 +0100

    Route failing issues has been fixed

---
+++ bootstrap/app.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+        using: function(){
+            Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+
+            Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
+
+            Route::middleware('api')
+            ->prefix('test')
+            ->group(base_path('routes/test.php'));
+        }
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
