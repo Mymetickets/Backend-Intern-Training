4e6222b7d39b5cb06c1d72efea6218107a1010bf
Willy-sam021 <s.o.williams021@gmail.com>
2025-06-26 15:03:01 +0100

    updated the event controller
    added the create method and the constructor. also imported the event form request and the eventbooking service

--- app/Http/Controllers/EventBookingController.php
+++ app/Http/Controllers/EventBookingController.php
@@ -1,17 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\EventBookingRequest;
+use App\Services\EventBookingService;
 use Illuminate\Http\Request;
 
 class EventBookingController extends Controller
 {
-    public function index(Request $request)
+
+    public $eventBookingService;
+    public function __construct()
+    {
+        $this->eventBookingService = new EventBookingService();
+    }
+
+
+    public function create(EventBookingRequest $request)
     {
 
+        $data = $this->eventBookingService->create_event($request);
         return response()->json([
             'message' => 'Event Created successfully',
-            
+
         ]);
     }
 }
