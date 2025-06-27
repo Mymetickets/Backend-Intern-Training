0ae38e740fa1d2da5cb27f701b778ac2e751a600
Willy-sam021 <s.o.williams021@gmail.com>
2025-06-27 09:03:48 +0100

    migration file added
    created the event booking migration file

--- app/Models/EventBooking.php
+++ app/Models/EventBooking.php
@@ -6,13 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class EventBooking extends Model
 {
-    protected $table = 'event_bookings';
-
+    
     protected $fillable = [
         'Name',
         'location',
         'date',
-        
+
     ];
 
 
